4b45d2f2fdaff20d3d26a0296bdf62a1f5f63165
Ashary Vermaysha <vermaysha@gmail.com>
2022-07-08 19:20:57 +0700

    create database

---
+++ database/index.js
@@ -0,0 +1,21 @@
+const {
+    Sequelize
+} = require('sequelize')
+const process = require('process')
+const { logger } = require('../helpers')
+require('dotenv').config()
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    logging: (sql, timing) => logger.info(sql, typeof timing === 'number' ? `Elapsed time: ${timing}ms` : ''),
+});
+
+const modelDefiners = [
+	require('./models/session.model')
+];
+
+// We define all models according to their files.
+for (const modelDefiner of modelDefiners) {
+	modelDefiner(sequelize);
+}
+
+module.exports = sequelize;
