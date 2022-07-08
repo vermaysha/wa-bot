9a50104f60b0a01659a32957f57b18be144e234b
Ashary Vermaysha <vermaysha@gmail.com>
2022-07-08 19:21:35 +0700

    Create main file

---
+++ index.js
@@ -0,0 +1,5 @@
+const { server } = require('./server');
+const { bot } = require('./bot')
+
+bot()
+// server()
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const process = require('process')
+const os = require('os')
+const app = express()
+const { logger } = require('./helpers')
+
+function format(seconds) {
+  function pad(s) {
+    return (s < 10 ? '0' : '') + s;
+  }
+  var hours = Math.floor(seconds / (60 * 60));
+  var minutes = Math.floor(seconds % (60 * 60) / 60);
+  var seconds = Math.floor(seconds % 60);
+
+  return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+}
+
+exports.server = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  app.get('/', (req, res) => {
+    res.json({
+      'api-status': 'online',
+      'bot-status': 'online/offline',
+      'app-uptime': format(process.uptime()),
+      'os-uptime': format(os.uptime())
+    })
+  })
+
+  logger.info('Server listen to port 3000')
+  app.listen(3000)
+}
